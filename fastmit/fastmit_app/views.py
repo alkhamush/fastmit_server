aabc2701e45ef44e1fe0beb059a9653830a5f38a
alkhamush <alkhamush@gmail.com>
2015-04-08 03:46:04 +0300

    registration

--- fastmit/fastmit_app/views.py
+++ fastmit/fastmit_app/views.py
@@ -3,8 +3,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
 from django.contrib import auth
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
+import json
 
 
 def registration(request):
-    return HttpResponse("registration")
+    username = 'first_name'
+    password = 'first_pass'
+    email = 'email'
+    if request.POST:
+        if User.objects.filter(username=username).exists():
+            return HttpResponseForbidden(json.dumps({"response": "User exists"}))
+        if User.objects.filter(username=email).exists():
+            return HttpResponseForbidden(json.dumps({"response": "Email is already registered"}))
+        user = User(username='first_name', password=password, email=email)
+        user.save()
+        return HttpResponse(json.dumps({"token": "token_for_user"}))

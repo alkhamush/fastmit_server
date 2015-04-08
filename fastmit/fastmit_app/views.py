0451123eb94581d11ffc6bb4326284c4d7c9fb65
alkhamush <alkhamush@gmail.com>
2015-04-08 03:57:40 +0300

    registration

--- fastmit/fastmit_app/views.py
+++ fastmit/fastmit_app/views.py
@@ -8,14 +8,14 @@ import json
 
 
 def registration(request):
-    username = 'first_name'
+    username = 'first_name2'
     password = 'first_pass'
-    email = 'email'
-    if request.POST:
-        if User.objects.filter(username=username).exists():
-            return HttpResponseForbidden(json.dumps({"response": "User exists"}))
-        if User.objects.filter(username=email).exists():
-            return HttpResponseForbidden(json.dumps({"response": "Email is already registered"}))
-        user = User(username='first_name', password=password, email=email)
-        user.save()
-        return HttpResponse(json.dumps({"token": "token_for_user"}))
+    email = 'email2'
+    #if request.POST:
+    if User.objects.filter(username=username).exists():
+        return HttpResponseForbidden(json.dumps({"response": "User exists"}))
+    if User.objects.filter(email=email).exists():
+        return HttpResponseForbidden(json.dumps({"response": "Email is already registered"}))
+    user = User(username=username, password=password, email=email)
+    user.save()
+    return HttpResponse(json.dumps({"token": "token_for_user"}))

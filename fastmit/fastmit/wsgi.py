3d92489db28ec1d66747980dcfb7c1453b62db8b
root <root@ubuntu.(none)>
2015-04-11 16:42:45 +0300

    settings to use gunicorn

--- fastmit/fastmit/wsgi.py
+++ fastmit/fastmit/wsgi.py
@@ -11,6 +11,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastmit.settings")
 
 
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 

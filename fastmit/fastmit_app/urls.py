87d8c9086754cf4a1076cc17f1cac2a0b21bd73a
nap <nap@ubuntu.(none)>
2015-04-12 15:18:18 +0300

    added registration, login, logout

--- fastmit/fastmit_app/urls.py
+++ fastmit/fastmit_app/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^some-secret-api/registration$', 'fastmit_app.views.registration', name='registration'),
+    url(r'^some-secret-api/login$', 'fastmit_app.views.login', name='login'),
+    url(r'^some-secret-api/logout$', 'fastmit_app.views.logout', name='logout'),
 )
--- fastmit/fastmit_app/views.py
+++ fastmit/fastmit_app/views.py
@@ -1,10 +1,71 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import User
-from django.contrib import auth
+import json
 from django.http import HttpResponse
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.db import IntegrityError
 
+def json_response(response_dict, status=200):
+    response = HttpResponse(json.dumps(response_dict), content_type="application/json", status=status)
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
 
 def registration(request):
-    return HttpResponse("registration")
+    if request.method == 'OPTIONS':
+        return json_response({})
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        if username is None or email is None or password is None:
+            return json_response({'response': 'Invalid data'}, status=403)
+        if User.objects.filter(email=email).count():
+            return json_response({'response': 'Email is already registered'}, status=403)
+        try:
+            User.objects.create_user(username=username, email=email, password=password)
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            session_key = request.session.session_key
+            return json_response({'token': session_key})
+        except IntegrityError:
+            return json_response({'response': 'User exists'}, status=403)
+    else:
+        return json_response({'response': 'Invalid method'}, status=403)
+
+def login(request):
+    if request.method == 'OPTIONS':
+        json_response({})
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        if username is None or password is None:
+            return json_response({'response': 'Wrong login or password'})
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                auth.login(request, user)
+                session_key = request.session.session_key
+                return json_response({'token': session_key})
+            else:
+                return json_response({'response': 'User is sleeping'}, status=403)
+        else:
+            return json_response({'response': 'Wrong login or password'}, status=403)
+    else:
+        return json_response({'response': 'Invalid method'}, status=403)
+
+def logout(request):
+    if request.method == 'OPTIONS':
+        json_response({})
+    elif request.method == 'POST':
+        token = request.POST.get('token', None)
+        if token is None:
+            return json_response({'response': 'Logout fail'}, status=403)
+        try:
+            Session.objects.get(pk=token).delete()
+            return json_response({'response': 'Ok'})
+        except Session.DoesNotExist:
+            return json_response({'response': 'Logout fail'}, status=403)
+    else:
+        return json_response({'response': 'Invalid method'}, status=403)

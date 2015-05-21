f8faae6b87053d05840c955ffc3d6b7567e4a400
Anis <alkhamush@fastmit.(none)>
2015-05-21 19:48:52 -0400

    chat: message sending for online to online

--- fastmit/tornadoapp.py
+++ fastmit/tornadoapp.py
@@ -1,5 +1,6 @@
 # coding=utf-8
-
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fastmit.settings")
 import json
 
 import tornado.ioloop
@@ -7,7 +8,7 @@ import tornado.web
 import tornado.websocket
 
 from tornado.options import define, options, parse_command_line
-
+from django.contrib.sessions.models import Session
 define("port", default=8888, type=int)
 
 
@@ -19,14 +20,20 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def open(self, *args):
         print "New connection"
+	session =  self.get_cookie("sessionid")
+	print session
+	session = Session.objects.get(session_key=session)
+	uid = session.get_decoded().get('_auth_user_id')
+	print uid
+	self.application.webSocketPool[uid] = self
         #self.write_message("Welcome!")
 
     def on_message(self, message):
         print message
-        message = json.loads(message)
-        message["body"]["id_message"] += "1" 
-        message = json.dumps(message)
-        self.write_message(message)
+        message2 = json.loads(message)
+        to = int(message2["body"]["id_friend"])
+	self.app.webSocketPool[to].write_message(message)
+        #self.write_message(message)
 
     def on_close(self):
         print "Connection closed"

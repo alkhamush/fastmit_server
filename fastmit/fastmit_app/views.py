bfe77fb8b0b3def128487850a98e9e0131a7ead4
alkhamush <alkhamush@gmail.com>
2015-08-22 00:18:37 +0300

    encryption: Added return of publicKey

--- fastmit/fastmit_app/views.py
+++ fastmit/fastmit_app/views.py
@@ -86,7 +86,9 @@ def friends(request):
         for friend_id in list_friend_id:
             friend = dict()
             friend['id'] = friend_id
-            friend['username'] = User.objects.get(pk=friend_id).username
+            user = User.objects.get(pk=friend_id)
+            friend['username'] = user.username
+            friend['publicKey'] = user.public_key.public_key
             friend['isOnline'] = False
             friend['photoUrl'] = r.get('user_%s_avatar' % friend_id)
             friend['hasUnread'] = len(r.zrange('messages_from_%s_to_%s' % (friend_id, uid), 0, -1, withscores=True)) > 0

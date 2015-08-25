908ed4b202b3bbb520f9c7defb25a6bb9b49cb5c
Anis Khamush <a.khamush@corp.mail.ru>
2015-08-26 02:28:52 +0300

    push: changed message

--- fastmit/chat/push.py
+++ fastmit/chat/push.py
@@ -8,5 +8,5 @@ def push_message(uid):
 
     reg_id = get_gcm_key(uid)
     if reg_id:
-        data = {'the_message': 'You have x new friends', 'param2': 'value2'}
-        API_KEY_GCM.plaintext_request(registration_id=reg_id, data=data)
+        data = {"message": "You have x new friends", "title": "Fastmit"}
+        API_KEY_GCM.json_request(registration_ids=[reg_id], data=data)
